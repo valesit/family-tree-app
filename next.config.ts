300aae1dbff5b32b81940c66bf74db12f5281e6e
valesit <valentinesithole@yahoo.com>
2026-09-09 10:56:51 -0400

    Fix optimized profile images by allowing the family Blob store (#21)

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "hbiw2z0a2ykd1udm.public.blob.vercel-storage.com",
+        port: "",
+        pathname: "/persons/**",
+        search: "",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
